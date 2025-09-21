c2a6d8d8dd27541dbeef4529a97df809af861b2b
Rohan9026 <rohan.cool18786@gmail.com>
2025-09-21 12:54:45 +0530

    changes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
+const baseURL = import.meta.env.MODE === 'development'
+  ? 'http://localhost:4000' // Your local backend URL
+  : 'https://chat-app-backend-1vh3.onrender.com'; // Your deployed backend URL
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:4000/api" : "/api",
-  withCredentials: true,
+  baseURL: baseURL,
+  withCredentials: true, // If you're using cookies for sessions
 });
