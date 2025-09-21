51f63beaec92401569ae5e3cd4aa9a25c84d741c
Rohan9026 <rohan.cool18786@gmail.com>
2025-09-21 13:55:17 +0530

    changes commited

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -6,8 +6,10 @@ const app = express();
 const server = http.createServer(app);
 
 const io = new Server(server, {
+
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: "https://chat-app-frontend-seven-pi.vercel.app", 
+    methods: ["GET", "POST"],
   },
 });
 
